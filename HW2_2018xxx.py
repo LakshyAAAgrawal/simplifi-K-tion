c280282ff8a23cd787a3b28b26f3154ff725577e
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2018-10-14 10:40:18 +0530

    Working Prime Impliants Function, but not don't care till now

--- HW2_2018xxx.py
+++ HW2_2018xxx.py
@@ -29,19 +29,20 @@ def dec_to_bin(numVar, num):
         hext='0'*(numVar-len(hext))+hext
     return(hext)
 
-def is_adjacent(numVar, num1, num2):
+def is_adjacent(num1, num2):
     '''
     Given 2 numbers(representing minterms), this function returns if they correspond to adjacet cells in k-map representation or not.
     return True if adjacent
     return False in all other cases
     '''
-    num1=dec_to_bin(numVar,num1)
-    num2=dec_to_bin(numVar,num2)
     i=0
     count=0
     while i<len(num1):
         if num1[i]!=num2[i]:
-            count=count+1
+            if num1[i]=='-' or num2[i]=='-':
+                return(False)
+            if not (num1[i]=='-' or num2[i]=='-'):
+                count=count+1
         i=i+1
         if count>=2:
             return(False)
@@ -50,6 +51,7 @@ def is_adjacent(numVar, num1, num2):
     else:
         return(False)
 
+'''
 def list_of_adj_pos(numVar, minterm):
     if numVar==1:
         return([(minterm+1)%2])
@@ -65,6 +67,16 @@ def list_of_adj_pos(numVar, minterm):
                     lis.append(i)
                 i=i+1
         return(lis)
+'''
+def minterms_as_bin(numVar, function_rep_as_list):
+    i=0
+    lis=[]
+    while i<len(function_rep_as_list):
+        if function_rep_as_list[i]==1 or function_rep_as_list[i]=='X':
+            lis.append(dec_to_bin(numVar, i))
+        i=i+1
+    return(lis)
+    
 
 def function_rep_as_list(numVar, str1):
     '''
@@ -85,6 +97,45 @@ def function_rep_as_list(numVar, str1):
         l[dont]='X'
     return(l)
 
+def comm_expr(str1, str2, str_to_add):
+    i=0
+    str_ret=''
+    while i<len(str1):
+        if str1[i]==str2[i]:
+            str_ret=str_ret + str1[i]
+        else:
+            str_ret=str_ret + str_to_add
+        i=i+1
+    return(str_ret)
+
+def func_list_of_prime_implicants(numVar, list_of_implicants, level=1):
+    i=0
+    lis_of_new_groups=[]
+    while i<len(list_of_implicants):
+        j=i
+        while j<len(list_of_implicants):
+            if is_adjacent(list_of_implicants[i], list_of_implicants[j]):
+                lis_of_new_groups.append(comm_expr(list_of_implicants[i], list_of_implicants[j], '*'))
+            j=j+1
+        i=i+1
+    for expr in lis_of_new_groups:
+        a=expr.replace('*','1')
+        if a in list_of_implicants:
+            list_of_implicants.remove(a)
+        a=expr.replace('*','0')
+        if a in list_of_implicants:
+            list_of_implicants.remove(a)
+        a=expr.replace('*', '-')
+        if not a in list_of_implicants:
+            list_of_implicants.append(a)
+    list_of_implicants.sort()
+    if level+1>numVar:
+        return(list_of_implicants)
+    else:
+        return(func_list_of_prime_implicants(numVar, list_of_implicants, level=level+1))
+
+
+
 def minFunc(numVar, stringIn):
 	"""
         This python function takes function of maximum of 4 variables
@@ -92,16 +143,18 @@ def minFunc(numVar, stringIn):
         as the output (minimized using the K-Map methodology),
         considering the case of Don’t Care conditions.
 
-	Input is a string of the format (a0,a1,a2, ...,an) d(d0,d1, ...,dm)
-	Output is a string representing the simplified Boolean Expression in
-	SOP form.
+        Input is a string of the format (a0,a1,a2, ...,an) d(d0,d1, ...,dm)
+        Output is a string representing the simplified Boolean Expression in
+        SOP form.
 
         No need for checking of invalid inputs.
         
-	Do not include any print statements in the function.
+        Do not include any print statements in the function.
 	"""
 	func_rep=function_rep_as_list(numVar, stringIn)
-	stringOut=str(func_rep)
+	list_of_minterms_single=minterms_as_bin(numVar, func_rep)
+	list_of_prime_implicants=func_list_of_prime_implicants(numVar, list_of_minterms_single, level=1)
+	stringOut=str(list_of_prime_implicants)
 	return stringOut
 
-print(minFunc(2, "(0) d(2)"))
+print(minFunc(4, "(0,1,2,4,5,12,13,8,9) d(5,14)"))
--- HW2_testing_2018xxx.py
+++ HW2_testing_2018xxx.py
@@ -1,11 +1,54 @@
 import unittest
 from HW2_2018xxx import minFunc
-
-
+from HW2_2018xxx import dec_to_bin
+from HW2_2018xxx import function_rep_as_list
+from HW2_2018xxx import comm_expr
+from HW2_2018xxx import is_adjacent
+from HW2_2018xxx import func_list_of_prime_implicants
 
 class testpoint(unittest.TestCase):
-	def test_minFunc(self):
-		
+    def test_dec_to_bin(self):
+        self.assertEqual(dec_to_bin(1,1),'1')
+        self.assertEqual(dec_to_bin(2,2),'10')
+        self.assertEqual(dec_to_bin(3,5),'101')
+        self.assertEqual(dec_to_bin(4,15),'1111')
+    
+    def test_function_rep_as_list(self):
+        self.assertEqual(function_rep_as_list(1,'(0) d()'), [1,0])
+        self.assertEqual(function_rep_as_list(2,'(2) d(1)'), [0,'X',1,0])
+        self.assertEqual(function_rep_as_list(3,'(5,3) d(7)'), [0,0,0,1,0,1,0,'X'])
+        self.assertEqual(function_rep_as_list(4,'(1,7,3,14) d(2,6)'), [0,1,'X',1,0,0,'X',1,0,0,0,0,0,0,1,0])
+        self.assertEqual(function_rep_as_list(3,'(0,2,4) d(7)'), [1,0,1,0,1,0,0,'X'])
+    
+    def test_comm_expr(self):
+        self.assertEqual(comm_expr('0010', '0000', '-'),'00-0')
+        self.assertEqual(comm_expr('001', '000', '-'),'00-')
+        self.assertEqual(comm_expr('--1-', '--0-', '-'),'----')
+        self.assertEqual(comm_expr('1-', '0-', '-'),'--')
+        self.assertEqual(comm_expr('01', '00', '-'),'0-')
+    
+    def test_is_adjacent(self):
+        self.assertEqual(is_adjacent('000','010'), True)
+        self.assertEqual(is_adjacent('--0','--1'), True)
+        self.assertEqual(is_adjacent('-0-','-01'), False)
+        self.assertEqual(is_adjacent('0','1'), True)
+        self.assertEqual(is_adjacent('011','111'), True)
+        self.assertEqual(is_adjacent('-1-','-0-'), True)
+        self.assertEqual(is_adjacent('-1-','-01'), False)
+        self.assertEqual(is_adjacent('0','1'), True)
+        self.assertEqual(is_adjacent('00-0','0-01'), False)
+        
+    def test_func_list_of_prime_implicants(self):
+        self.assertEqual(func_list_of_prime_implicants(4,['0000','0001','0010','0100','0101','1000','1001','1100','1101','1110']),['--0-','00-0','11-0'])
+        self.assertEqual(func_list_of_prime_implicants(4,['0000','0001','0010','0011','0100','0101','0110','0111','1000','1001','1010','1011','1100','1101','1110','1111']),['----'])
+        self.assertEqual(func_list_of_prime_implicants(4,['0000','0001','0010','0011','0100','0101','0110','0111','1000','1001','1010','1011','1100','1101','1110']),['---0','--0-','-0--','0---'])
+        self.assertEqual(func_list_of_prime_implicants(4,['0000','0001','0010','0011','0100','0101','0110','0111','1000','1001','1010','1011','1100','1101','1110']),['---0','--0-','-0--','0---'])
+        self.assertEqual(func_list_of_prime_implicants(3,['000','001','010','011','100','101','110','111']),['---'])
+        self.assertEqual(func_list_of_prime_implicants(3,['011','101','110']),['011','101','110'])
+        self.assertEqual(func_list_of_prime_implicants(2,['00','01','10','11']),['--'])
+        
+    def test_minFunc(self):
+        None
                 
 if __name__=='__main__':
 	unittest.main()
