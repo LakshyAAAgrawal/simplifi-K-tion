913b8bdedcb979f598c53ee4d041b1f5147dc18d
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2019-03-21 23:17:13 +0530

    Full working code

--- HW2_2018xxx.py
+++ HW2_2018xxx.py
@@ -4,10 +4,14 @@
 # Roll Number:  2018242
 # Section:  B
 # Group: 3
-# Date: 11 October 2018
+# Date: 16 October 2018
+
+#import statements
+import copy
 
 def dec_to_bin(numVar, num):
     '''
+    Input : numVar - Number of variables in boolean function/Number of digits required in decimal representation, num=Number to be represented in Binary
     Returns a string in given number of digits(numVar) representing num in binary
     '''
     i=0
@@ -31,7 +35,10 @@ def dec_to_bin(numVar, num):
 
 def is_adjacent(num1, num2):
     '''
-    Given 2 numbers(representing minterms), this function returns if they correspond to adjacet cells in k-map representation or not.
+    Input : 2 strings consisting of numVar number of characters, each either of ('0','1','X')
+    
+    Given 2 strings(representing minterms, or prime implicants), this function returns if they correspond to adjacet groups in k-map representation or not. Alternatively, this function returns if 2 implicants(rep by num1, num2) can be combined.
+    
     return True if adjacent
     return False in all other cases
     '''
@@ -51,24 +58,12 @@ def is_adjacent(num1, num2):
     else:
         return(False)
 
-'''
-def list_of_adj_pos(numVar, minterm):
-    if numVar==1:
-        return([(minterm+1)%2])
-    else:
-        lis=[]
-        i=0
-        while i<2**numVar:
-            if i==minterm:
-                i=i+1
-                continue
-            else:
-                if is_adjacent(numVar, i, minterm):
-                    lis.append(i)
-                i=i+1
-        return(lis)
-'''
 def minterms_as_bin(numVar, function_rep_as_list, match_against):
+    '''
+    Input : numVar = Number of varibales in boolean function, function_rep_as_list = A list consisting of 2^numVar elements, with each element corresponding to the functions value at that minterm(0, 1 or 'X' for dont care), match_against = the character to match against to form the list (0, 1, or 'X')
+    
+    Returns a list of minterms(as binary numbers) corresponding to matching terms
+    '''
     i=0
     lis=[]
     while i<len(function_rep_as_list):
@@ -80,18 +75,18 @@ def minterms_as_bin(numVar, function_rep_as_list, match_against):
 
 def function_rep_as_list(numVar, str1):
     '''
-    Given the number of variables and string containing function representation, this function returns a list depicting the function, whose corresponfing elements correspond to the function minterms
+    Given the number of variables(numVar) and string containing function representation(str1), this function returns a list depicting the function, whose corresponfing elements correspond to the function minterms and have value(0,1 or 'X' for dont care)
     '''
     l=[]
     for i in range(0, 2**numVar):
         l.append(0)
-    minterms, dont_care=str1.split()
+    minterms, useless, dont_care=str1.split()
     if not len(minterms)==2:
         minterms=list(map(int, minterms[1:-1].split(',')))
     else:
         minterms=[]
-    if not len(dont_care)==3:
-        dont_care=list(map(int, dont_care[2:-1].split(',')))
+    if not len(dont_care)==1:
+        dont_care=list(map(int, dont_care[1:-1].split(',')))
     else:
         dont_care=[]
     for minterm in minterms:
@@ -101,6 +96,12 @@ def function_rep_as_list(numVar, str1):
     return(l)
 
 def comm_expr(str1, str2, str_to_add):
+    '''
+    Input : 2 strigs, str1 and str2, which correspond to binary representation of adjacent implicants in a k-map, i.e. is_adjacent() returns True. 
+            str_to_add is the character to replace the 0 and 1 of the common variables with.
+    
+    Returns a string of same length as str1 and str2, which corresponds as an implicant to the implicants described by both of str1 and str2, with the combinable variable represented by the character in str_to_add
+    '''
     i=0
     str_ret=''
     while i<len(str1):
@@ -112,6 +113,13 @@ def comm_expr(str1, str2, str_to_add):
     return(str_ret)
 
 def func_list_of_prime_implicants(numVar, list_of_implicants, level=1):
+    '''
+    Input : numVar = number of variables in boolean function, list_of_implicants = This takes in a list of minterms to be combined(consisting of both, 1 and 'X', as Don't cares can be used to simplify)
+    
+    This is a recursive function which calls itself till the depth reaches the number of variables present in function. It iterates over each element in the current list_of_implicants and matches them against each other, checking if they can be combined. After all the possible terms have been combined, the function calls itself with the current list for the next iteration. This way, when the last depth level is reached, all the elements left in the list are only prime implicants. This acts as an auxiliary function for the function prime_implicants()
+    
+    Returns a list of prime implicants for the function, assuming all the elements in the original list are minterms.
+    '''
     i=0
     lis_of_new_groups=[]
     while i<len(list_of_implicants):
@@ -138,6 +146,17 @@ def func_list_of_prime_implicants(numVar, list_of_implicants, level=1):
         return(func_list_of_prime_implicants(numVar, list_of_implicants, level=level+1))
 
 def prime_implicants(numVar, func_rep_as_list):
+    '''
+    Input : numVar = Number of varibales in boolean function, function_rep_as_list = A list consisting of 2^numVar elements, with each element corresponding to the functions value at that minterm(0, 1 or 'X' for dont care)
+    
+    This function returns a list of all prime implicants of the function whose representation is passed.
+    It first calls the function func_list_of_prime_implicants with all minterms and dont cares. 
+    Then it calls the same function with a list of Dont cares.
+    Then, it removes all the prime implicants which cover only the don't cares, from the list of prime implicants to be returned.
+    The final list thus obtained is a list of prime implicants only, for the given function.
+    
+    Returns a list of prime implicants corresponding to the given function, taking into account the presence of Don't cares.
+    '''
     list_of_minterms_only_dont_care=minterms_as_bin(numVar, func_rep_as_list, 'X')
     list_of_minterms_incl_dont_care=minterms_as_bin(numVar, func_rep_as_list, 1) + list_of_minterms_only_dont_care
     list_of_minterms_incl_dont_care.sort()
@@ -149,6 +168,12 @@ def prime_implicants(numVar, func_rep_as_list):
     return(list_of_all_prime_implicants)
 
 def minterm_belong_to_prime(minterm, prime_implicant):
+    '''
+    Input : minterm = A string representing a minterm in binary form, prime_implicant = A string representing a prime_implicant in the form '01--' where '-' represents the combined variables.
+    
+    Returns True if the minterm is covered  by the prime implicant
+    Returns False in all other cases
+    '''
     i=0
     while i<len(prime_implicant):
         if prime_implicant[i]=='-':
@@ -161,31 +186,147 @@ def minterm_belong_to_prime(minterm, prime_implicant):
     return(True)
 
 def prime_implicants_by_minterms(numVar, list_of_prime_implicants, func_rep_as_list):
+    '''
+    Input : numVar = Number of varibales in boolean function, list_of_prime_implicants = a list of all the prime implicants which cover the minterms in the function(as obtained from the function prime_implicants()),function_rep_as_list = A list consisting of 2^numVar elements, with each element corresponding to the functions value at that minterm(0, 1 or 'X' for dont care)
+    
+    Returns a list of lists, with each element being a list of prime implicants, which cover the minterm present at the same index in the list - list_of_minterms as obtained from minterms_as_bin
+    '''
     list_of_minterms=minterms_as_bin(numVar, func_rep_as_list, 1)
-    dict_of_prime_implicant_by_minterm={}
-    for i in list_of_minterms:
-        dict_of_prime_implicant_by_minterm[i]=[]
-        for j in list_of_prime_implicants:
-            if minterm_belong_to_prime(i, j):
-                dict_of_prime_implicant_by_minterm[i].append(j)
-    return(dict_of_prime_implicant_by_minterm)
+    list_of_prime_implicant_by_minterm=[]
+    i=0
+    while i<len(list_of_minterms):
+        a=[]
+        j=0
+        while j<len(list_of_prime_implicants):
+            if minterm_belong_to_prime(list_of_minterms[i], list_of_prime_implicants[j]):
+               a.append(list_of_prime_implicants[j])
+            j=j+1
+        list_of_prime_implicant_by_minterm.append(a)
+        i=i+1
+    return(list_of_prime_implicant_by_minterm)
+                
+def list_of_all_possible_solutions(lis_of_prime_implicant, prime_imp_by_minterms, list_of_sol, level=0, solution=[]):
+    '''
+    Input : lis_of_prime_implicant = A list of prime implicants of the function to be minimized(as obtained from prime_implicants)
+            prime_imp_by_minterms = A list of lists, with each element being a list of prime implicants, which cover the minterm present at the same index in the list - list_of_minterms as obtained from minterms_as_bin (as obtained from prime_implicants_by_minterms)
+            list_of_sol = The list to which the list of possible solutions should be appended. It should be an empty list for all practical purposes.
+            Others are for internal use.
+    
+    This function generates a list of lists, with each element(list) representing a possible solution for the given function. For this, it recursively generates a list of all prime_implicants that must be included to cover all the minterms. All the solutions are appended to the list supplied under list_of_sol
+    '''
+    if level==0:
+        for i in prime_imp_by_minterms[level]:
+            a=[]
+            a.append(lis_of_prime_implicant.index(i))
+            list_of_all_possible_solutions(lis_of_prime_implicant, prime_imp_by_minterms, list_of_sol, level=level+1, solution=a)
+    elif 0<level<len(prime_imp_by_minterms):
+        for i in prime_imp_by_minterms[level]:
+            c=copy.deepcopy(solution)
+            if not lis_of_prime_implicant.index(i) in c:
+                c.append(lis_of_prime_implicant.index(i))
+            list_of_all_possible_solutions(lis_of_prime_implicant, prime_imp_by_minterms, list_of_sol, level=level+1, solution=c) 
+    else:
+        solution.sort()
+        if not solution in list_of_sol:
+            list_of_sol.append(solution)
+def reduce_to_min_number_of_implicants(list_of_all_solutions):
+    '''
+    Input : list_of_all_solutions = a list of lists, with each element(list) representing a possible solution for the given function(as obtained from                                                                   list_of_all_possible_solutions())
+    
+    This function first compares the number of terms required by each solution in the list_of_all_solutions, and then finds the smallest number of terms required.
+    It then returns a list of lists(representing solutions) which contains the smallest number of terms in the soltion possible.
+    '''
+    smallest=10000000
+    for i in list_of_all_solutions:
+        if len(i)<smallest:
+            smallest=len(i)
+    final=[]
+    for i in list_of_all_solutions:
+        if len(i)==smallest:
+            final.append(i)
+    return(final)
+
+def solution_as_string(list_of_prime_implicant, solution):
+    '''
+    Input : list_of_all_prime_implicant = list of prime implicants covering the functions(as obtained from prime_implicants), solution = A list consisting of indices corresponding to prime implicants in the list_of_prime_implicants.
+    
+    Returns a list of strings, with each string containing one term of the function SOP, example - "wx'y" 
+    '''
+    list_of_terms=[]
+    for i in solution:
+        a=list_of_prime_implicant[i]
+        j=0
+        str_to_add=''
+        while j<len(a):
+            if a[j]=='-':
+                None
+            elif a[j]=='0':
+                str_to_add=str_to_add+['w','x','y','z', 'a', 'b', 'c'][j]+'\''
+            else:
+                str_to_add=str_to_add+['w','x','y','z', 'a', 'b', 'c'][j]
+            j=j+1
+        list_of_terms.append(str_to_add)
+    return(list_of_terms)
+
+def number_of_literals(list_of_prime_implicants, solution):
+    '''
+    Input : list_of_all_prime_implicant = list of prime implicants covering the functions(as obtained from prime_implicants), solution = A list consisting of indices corresponding to prime implicants in the list_of_prime_implicants.
+    
+    Returns the number of different literals present in the given solution. For this purpose, "w" and "w'" are treated as separate literals.
+    '''
+    literal=[]
+    for i in solution:
+        a=list_of_prime_implicants[i]
+        j=0
+        str_to_add=''
+        while j<len(a):
+            if a[j]=='-':
+                None
+            elif a[j]=='0':
+                lite=['w','x','y','z','a', 'b', 'c'][j]+'\''
+                if not lite in literal:
+                    literal.append(lite)
+            else:
+                lite=['w','x','y','z', 'a', 'b', 'c'][j]
+                if not lite in literal:
+                    literal.append(lite)
+            j=j+1
+    return(len(literal))
+        
+def red_based_on_number_of_literals(list_of_prime_implicant, lis_of_solutions):
+    '''
+    Input : list_of_all_prime_implicant = list of prime implicants covering the functions(as obtained from prime_implicants), lis_of_solutions = a list of lists, with each element(list) representing a possible solution for the given function(as obtained after reducing by the number of terms by the function reduce_to_min_number_of_implicants)
+    
+    This function first checks the number of literals used in each solution and finds the smallest number of literals used in any solution. It then makes a list of all solutions containing the smallest number of literals.
+    
+    Returns a list of lists, with each element representing a solution after being reduced for based on number of literals.
+    '''
+    smallest=10000000
+    red_list=[]
+    for i in lis_of_solutions:
+        z=number_of_literals(list_of_prime_implicant, i)
+        if z<smallest:
+            smallest=z
+    for i in lis_of_solutions:
+        if number_of_literals(list_of_prime_implicant, i)==smallest:
+            red_list.append(i)
+    return(red_list)
 
 def minFunc(numVar, stringIn):
-	"""
-        This python function takes function of maximum of 4 variables
-        as input and gives the corresponding minimized function(s)
-        as the output (minimized using the K-Map methodology),
-        considering the case of Don’t Care conditions.
-
-        Input is a string of the format (a0,a1,a2, ...,an) d(d0,d1, ...,dm)
-        Output is a string representing the simplified Boolean Expression in
-        SOP form.
-
-        No need for checking of invalid inputs.
-        
-        Do not include any print statements in the function.
-	"""
 	func_rep=function_rep_as_list(numVar, stringIn)
 	list_of_prime_implicants=prime_implicants(numVar, func_rep)
-	stringOut=str(list_of_prime_implicants)
-	return stringOut
+	if len(list_of_prime_implicants)==0:
+	     return('0')
+	lis_of_prime_implicant_by_minterm=prime_implicants_by_minterms(numVar, list_of_prime_implicants, func_rep)
+	list_of_solutions=[]
+	list_of_all_possible_solutions(list_of_prime_implicants, lis_of_prime_implicant_by_minterm, list_of_solutions)
+	reduced_list_of_all_possible_solutions_by_number_of_minterms=reduce_to_min_number_of_implicants(list_of_solutions)
+	red_list_based_on_number_of_literals=red_based_on_number_of_literals(list_of_prime_implicants, reduced_list_of_all_possible_solutions_by_number_of_minterms)
+	final_solution=red_list_based_on_number_of_literals[0]
+	if list_of_prime_implicants[final_solution[0]]==('-'*numVar):
+	     return('1')
+	
+	final_solution=[]
+	for i in red_list_based_on_number_of_literals:
+	     final_solution.append('+'.join(sorted(solution_as_string(list_of_prime_implicants, i))))
+	return ' OR '.join(sorted(final_solution))
--- HW2_testing_2018xxx.py
+++ HW2_testing_2018xxx.py
@@ -1,36 +1,61 @@
+# CSE 101 - IP HW2
+# K-Map Minimization 
+# Test File
+# Name: Lakshya A Agrawal
+# Roll Number:  2018242
+# Section:  B
+# Group: 3
+# Date: 16 October 2018
+
 import unittest
-from HW2_2018xxx import minFunc
-from HW2_2018xxx import dec_to_bin
-from HW2_2018xxx import function_rep_as_list
-from HW2_2018xxx import comm_expr
-from HW2_2018xxx import is_adjacent
-from HW2_2018xxx import func_list_of_prime_implicants
-from HW2_2018xxx import prime_implicants
-from HW2_2018xxx import minterm_belong_to_prime
-from HW2_2018xxx import prime_implicants_by_minterms
+from HW2_2018242 import minFunc
+from HW2_2018242 import dec_to_bin
+from HW2_2018242 import function_rep_as_list
+from HW2_2018242 import comm_expr
+from HW2_2018242 import is_adjacent
+from HW2_2018242 import func_list_of_prime_implicants
+from HW2_2018242 import prime_implicants
+from HW2_2018242 import minterm_belong_to_prime
+from HW2_2018242 import prime_implicants_by_minterms
+from HW2_2018242 import number_of_literals
 
 class testpoint(unittest.TestCase):
     def test_dec_to_bin(self):
+        '''
+        Test the function to generate binary representation of a number in given number of digits.
+        '''
         self.assertEqual(dec_to_bin(1,1),'1')
         self.assertEqual(dec_to_bin(2,2),'10')
         self.assertEqual(dec_to_bin(3,5),'101')
         self.assertEqual(dec_to_bin(4,15),'1111')
+        self.assertEqual(dec_to_bin(4,10),'1010')
     
     def test_function_rep_as_list(self):
-        self.assertEqual(function_rep_as_list(1,'(0) d()'), [1,0])
-        self.assertEqual(function_rep_as_list(2,'(2) d(1)'), [0,'X',1,0])
-        self.assertEqual(function_rep_as_list(3,'(5,3) d(7)'), [0,0,0,1,0,1,0,'X'])
-        self.assertEqual(function_rep_as_list(4,'(1,7,3,14) d(2,6)'), [0,1,'X',1,0,0,'X',1,0,0,0,0,0,0,1,0])
-        self.assertEqual(function_rep_as_list(3,'(0,2,4) d(7)'), [1,0,1,0,1,0,0,'X'])
+        '''
+        Tests the function used to parse user input for processing by the function minFunc
+        '''
+        self.assertEqual(function_rep_as_list(1,'(0) d -'), [1,0])
+        self.assertEqual(function_rep_as_list(2,'(2) d (1)'), [0,'X',1,0])
+        self.assertEqual(function_rep_as_list(3,'(5,3) d (7)'), [0,0,0,1,0,1,0,'X'])
+        self.assertEqual(function_rep_as_list(4,'(1,7,3,14) d (2,6)'), [0,1,'X',1,0,0,'X',1,0,0,0,0,0,0,1,0])
+        self.assertEqual(function_rep_as_list(3,'(0,2,4) d (7)'), [1,0,1,0,1,0,0,'X'])
     
     def test_comm_expr(self):
+        '''
+        Tests the function to generate a common expression for 2 given implicants which are adjacent to each other
+        '''
         self.assertEqual(comm_expr('0010', '0000', '-'),'00-0')
+        self.assertEqual(comm_expr('001-', '000-', '*'),'00*-')
         self.assertEqual(comm_expr('001', '000', '-'),'00-')
         self.assertEqual(comm_expr('--1-', '--0-', '-'),'----')
         self.assertEqual(comm_expr('1-', '0-', '-'),'--')
         self.assertEqual(comm_expr('01', '00', '-'),'0-')
+        self.assertEqual(comm_expr('1', '0', '-'),'-')
     
     def test_is_adjacent(self):
+        '''
+        Tests the function which checks whether 2 given implicants are adjacent(can be combined) or not
+        '''
         self.assertEqual(is_adjacent('000','010'), True)
         self.assertEqual(is_adjacent('--0','--1'), True)
         self.assertEqual(is_adjacent('-0-','-01'), False)
@@ -49,8 +74,8 @@ class testpoint(unittest.TestCase):
         self.assertEqual(minterm_belong_to_prime('01', '-1'), True)
     
     def test_prime_implicants_by_minterms(self):
-        self.assertEqual(prime_implicants_by_minterms(4, ['-1-0', '-11-', '0-01', '01--', '1-10'],[0,'X',0,0,1,1,1,'X',0,'X',1,0,'X',0,1,'X']), {'0100':['-1-0','01--'], '0101':['0-01','01--'], '0110':['-1-0', '-11-', '01--'], '1010':['1-10'], '1110':['-1-0', '-11-', '1-10']})
-        self.assertEqual(prime_implicants_by_minterms(4,['-001', '-010', '-100', '-111', '0-10', '01-0', '011-', '1-0-', '10-0', '11-1'],[0,'X','X',0,'X',0,1,1,1,1,1,0,1,'X',0,1]),{'0110':['0-10','01-0','011-'],'0111':['-111','011-'],'1000':['1-0-','10-0'],'1001':['-001','1-0-'], '1010':['-010','10-0'], '1100':['-100','1-0-'], '1111':['-111', '11-1']})
+        self.assertEqual(prime_implicants_by_minterms(4, ['-1-0', '-11-', '0-01', '01--', '1-10'],[0,'X',0,0,1,1,1,'X',0,'X',1,0,'X',0,1,'X']), [['-1-0','01--'],['0-01','01--'],['-1-0', '-11-', '01--'],['1-10'],['-1-0', '-11-', '1-10']])
+        self.assertEqual(prime_implicants_by_minterms(4,['-001', '-010', '-100', '-111', '0-10', '01-0', '011-', '1-0-', '10-0', '11-1'],[0,'X','X',0,'X',0,1,1,1,1,1,0,1,'X',0,1]),[['0-10','01-0','011-'],['-111','011-'],['1-0-','10-0'],['-001','1-0-'],['-010','10-0'],['-100','1-0-'],['-111', '11-1']])
         
     def test_func_list_of_prime_implicants(self):
         self.assertEqual(func_list_of_prime_implicants(4,['0000','0001','0010','0100','0101','1000','1001','1100','1101','1110']),['--0-','00-0','11-0'])
@@ -60,7 +85,10 @@ class testpoint(unittest.TestCase):
         self.assertEqual(func_list_of_prime_implicants(3,['000','001','010','011','100','101','110','111']),['---'])
         self.assertEqual(func_list_of_prime_implicants(3,['011','101','110']),['011','101','110'])
         self.assertEqual(func_list_of_prime_implicants(2,['00','01','10','11']),['--'])
-        
+    
+    def test_number_of_literals(self):
+        self.assertEqual(number_of_literals(['--00', '--11', '-0-1', '-00-', '-1-0', '-11-', '1--1', '1-0-', '11--'], [2, 1, 0, 8]), 7)
+    
     def test_prime_implicants(self):
         self.assertEqual(prime_implicants(4,[0,0,0,0,0,'X',0,'X',0,0,'X',0,0,'X',0,0]),[])
         self.assertEqual(prime_implicants(4,[1,1,1,0,1,'X',0,0,1,1,0,0,1,1,'X',0]),['--0-','00-0','11-0'])
@@ -73,11 +101,75 @@ class testpoint(unittest.TestCase):
         self.assertEqual(prime_implicants(2,[1,'X',1,'X']),['--'])
         self.assertEqual(prime_implicants(1,[0,'X']),[])
         self.assertEqual(prime_implicants(1,[1,0]),['0'])
-    #below test case is for reduced expression
-    #self.assertEqual(prime_implicants(4,[0,1,0,0,'X',1,1,0,1,0,1,'X',0,'X','X',1]),['01-0','1-1-'])
         
     def test_minFunc(self):
-        None
+        '''
+        Each of the Test case has been personally verified and also verified using other tools. In some cases, the number of solutions provided by minFunc is lesser than the ones provided by other tools. On manual testing, it was found that the solutions provided by minFunc used lesser number of literals as compared to the ones provided by other tools(minFunc treats "w" and "w'" as separate literals, as "w'" needs to be computed separately, therefore increasing the number of gates required, and therefore the cost of implementation), and therefore, didn't result in a loss of any solutions.
+        '''
+        
+        #Testing of 4 variable Functions with don't cares
+        self.assertEqual(minFunc(4, '(3,6,10,13) d (0,2,5,7,8,9,12)'), "w'y+wy'+x'z'")
+        self.assertEqual(minFunc(4, '(0,1) d (2,3,4,5,6,8,9,10,12,13,14)'), "y'")
+        self.assertEqual(minFunc(4, '(6,11) d (0,3,4,5,8,10,12,14)'), "wx'y+xz' OR x'yz+xz'")
+        self.assertEqual(minFunc(4, '(0,2,5,6,11,13) d (1,3,7,9,10,12,14,15)'), "w'x'+y+z")
+        self.assertEqual(minFunc(4, '(3,9,12,15) d (0,1,4,7,8,10,11,14)'), "wx'+wz'+yz OR wy+wz'+x'z OR wz'+x'z+yz OR x'y'+y'z'+yz OR x'z+y'z'+yz")
+        self.assertEqual(minFunc(4, '(1,2,3,7,9,11,12,14) d (0,6,13,15)'), "w'y+wx+x'z")
+        self.assertEqual(minFunc(4, '(1,2,8,10,15) d (0,4,6,11,12)'), "w'x'y'+wyz+x'z'")
+        self.assertEqual(minFunc(4, '(2,8,10,12) d (1,3,4,6,7,9,11,13,15)'), "wy'+x'y")
+        
+        #Testing of 4 variable Functions without Don't cares
+        self.assertEqual(minFunc(4, '(0,1,2,4,5,6,8,9,12,13,14) d -'), "w'z'+xz'+y'")
+        self.assertEqual(minFunc(4, '(1,2,4,5,6,10,11,12,13) d -'), "w'y'z+w'yz'+wx'y+xy'")
+        self.assertEqual(minFunc(4, '(2,3,4,5,8,9,10,15) d -'), "w'x'y+w'xy'+wx'y'+wx'z'+wxyz OR w'x'y+w'xy'+wx'y'+wxyz+x'yz'")
+        self.assertEqual(minFunc(4, '(0,1,4,5,7,9,10,11,13,15) d -'), "w'y'+wx'y+wz+xz OR w'y'+wx'y+xz+y'z")
+        
+        #Testing of 3 variable Functions with Don't Cares
+        self.assertEqual(minFunc(3, '(3,4,7) d (1,2,5,6)'), 'w+x OR w+y')
+        self.assertEqual(minFunc(3, '(5) d (2,3,6)'), "wx'y")
+        self.assertEqual(minFunc(3, '(0,5) d (2,3,4)'), "wx'+x'y'")
+        self.assertEqual(minFunc(3, '(0,1,5,7) d (6)'), "w'x'+wy")
+        self.assertEqual(minFunc(3, '(2,5) d (0,4,6)'), "wx'+y'")
+        self.assertEqual(minFunc(3, '(3,5) d (1,2,6)'), "w'y+x'y")
+        
+        #Testing of 3 variable Functions without Don't Cares
+        self.assertEqual(minFunc(3, '(3) d -'), "w'xy")
+        self.assertEqual(minFunc(3, '(2,3,6,7) d -'), "x")
+        self.assertEqual(minFunc(3, '(0,1,2,3,7) d -'), "w'+xy")
+        self.assertEqual(minFunc(3, '(0,1,2,5,6,7) d -'), "w'x'+wy+xy' OR w'y'+wx+x'y")
+        self.assertEqual(minFunc(3, '(0,3,4,5,7) d -'), "wx'+x'y'+xy OR wy+x'y'+xy")
+        
+        #Testing of 2 variable Functions with Don't Cares
+        self.assertEqual(minFunc(2, '() d (0)'), "0")
+        self.assertEqual(minFunc(2, '(1,2) d (3)'), "w+x")
+        self.assertEqual(minFunc(2, '(1) d (0,3)'), "w' OR x")
+        self.assertEqual(minFunc(2, '(3) d (0)'), "wx")
+        
+        #Testing of 2 variable Functions without Don't Cares
+        self.assertEqual(minFunc(2, '(1,2) d -'), "w'x+wx'")
+        self.assertEqual(minFunc(2, '(3) d -'), "wx")
+        
+        #Testing of single variable functions
+        self.assertEqual(minFunc(1, '(1) d (0)'), "1")
+        self.assertEqual(minFunc(1, '(0,1) d -'), "1")
+        self.assertEqual(minFunc(1, '(0) d -'), "w'")
+        
+        #Border Cases - Zero Function
+        self.assertEqual(minFunc(4, '() d -'), '0')
+        self.assertEqual(minFunc(3, '() d -'), '0')
+        self.assertEqual(minFunc(2, '() d -'), '0')
+        self.assertEqual(minFunc(1, '() d -'), '0')
+        self.assertEqual(minFunc(4, '() d (1,4,9,15)'), '0')
+        self.assertEqual(minFunc(3, '() d (0,3,7)'), '0')
+        self.assertEqual(minFunc(2, '() d (2,3)'), '0')
+        self.assertEqual(minFunc(1, '() d (1)'), '0')
+        
+        #Border Cases - One Function
+        self.assertEqual(minFunc(4, '(0,1,2,3,4,5,6) d (7,8,9,10,11,12,13,14,15)'), '1')
+        self.assertEqual(minFunc(3, '(0,2,4,6) d (1,3,5,7)'), '1')
+        self.assertEqual(minFunc(2, '(1,3) d (0,2)'), '1')
+        self.assertEqual(minFunc(1, '(0) d (1)'), '1')
+        
+        #self.assertEqual(minFunc(6,'(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,61,62,63) d -'), '1')
                 
 if __name__=='__main__':
 	unittest.main()
